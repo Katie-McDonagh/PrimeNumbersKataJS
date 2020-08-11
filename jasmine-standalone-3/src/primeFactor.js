dcef341229ca49146bb91e2a7f842e4561d086ab
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-11 15:28:36 +0100

    green test -returns [2,2] when given 4

--- jasmine-standalone-3/src/primeFactor.js
+++ jasmine-standalone-3/src/primeFactor.js
@@ -1,6 +1,12 @@
 function primeFactorList(n){
-  if(n>1){
-    return [n]
+  var listArray = []
+  if(n > 1){
+    if(n % 2 === 0){
+      listArray.push(2)
+      n /= 2
+    }
+    if(n > 1)
+    listArray.push(n)
   };
-  return []
+  return listArray
 };
